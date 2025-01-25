efee5a056f0fce9dbfc2ba11452036d8dbe04486
Karol Szpakowski <karol.szpakowski@gmail.com>
2025-01-25 01:49:56 +0100

    fix secrets creation

--- mgmt-cluster/create.sh
+++ mgmt-cluster/create.sh
@@ -4,6 +4,7 @@ kubectl create ns argocd
 kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.14.0-rc5/manifests/install.yaml
 kubectl -n argocd rollout status deployment argocd-server
 kubectl apply -f apps.yaml
+kubectl create ns crossplane-system
 kubectl apply -f secrets/
 kubectl -n argocd port-forward svc/argocd-server 8080:443 &
 open https://localhost:8080
